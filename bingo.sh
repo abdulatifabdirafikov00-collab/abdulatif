1e58afc3af4e93687e7685e2f79b8399ede947b5
Abdulatif <abdulatifabdirafikov00@gmail.com>
2026-08-14 11:53:18 +0500

    Add bingo script

---
+++ bingo.sh
@@ -0,0 +1,25 @@
+NUMBER=$RANDOM
+echo $NUMBER
+
+
+#!/bin/bash
+# Bingo Number Generator
+echo -e "\n~~ Bingo Number Generator ~~\n"
+
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
+elif [[ $NUMBER -lt 61 ]]
+then
+  echo $TEXT G:$NUMBER
+else
+  echo $TEXT O:$NUMBER
+fi
+
+
